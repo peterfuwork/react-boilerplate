a762d99e799e2ee8d70d340f784ef21756b7fbb6
Chiaen Fu <peterfuwork@gmail.com>
2018-12-06 13:13:14 -0700

    added the font-awesome icons

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         Hello World!<br/>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star-o"></span>
+        <span class="fa fa-star-half-o"></span>
         <img className="App-logo" src={image} />
       </div>
     );
